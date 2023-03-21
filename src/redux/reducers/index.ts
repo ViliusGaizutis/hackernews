62c0dd0986c75540f9f2a5a32d9beec164ae74c4
ViliusGaizutis <viliusgaizutis@coherentsolutions.com>
2023-03-21 11:43:46 +0200

    set up initial boilerplate

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,5 @@
+import { combineReducers, Reducer, CombinedState, AnyAction } from "redux";
+
+const rootReducer: Reducer<CombinedState<{}>, AnyAction> = combineReducers({});
+
+export default rootReducer;
